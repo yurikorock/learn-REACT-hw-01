6418479d8bf44320d170c673029f23d0a7495d2a
yurii macbook pro <yurikorock777@gmail.com>
2025-08-03 15:52:54 +0300

    add modern normalize

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import  Profile  from '../Profile/Profile.jsx';
-import  userData  from '../../../userData.json';
+import  userData  from '../../userData.json';
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "modern-normalize";
 import './index.css'
-import App from './assets/components/App/App.jsx'
+import App from './components/App/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
