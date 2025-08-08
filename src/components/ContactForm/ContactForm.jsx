c7ca471ab6dcd3e22a85a957bbc5c3e7fe8bc92d
yurii macbook pro <yurikorock777@gmail.com>
2025-08-08 08:36:11 +0300

    validate add contact

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,7 @@
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
 import { useId } from 'react';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
+import * as yup from 'yup';
 
 export default function ContactForm({ onAdd }) {
   const nameFieldId = useId();
@@ -10,18 +11,36 @@ export default function ContactForm({ onAdd }) {
     onAdd({
       id: nanoid(),
       name: values.name,
-      number: values.number
-    })
+      number: values.number,
+    });
     actions.resetForm();
   };
+  const validateSchema = yup.object({
+    name: yup
+      .string()
+      .min(3, 'Too short!')
+      .max(50, 'Too long!')
+      .required('Required!'),
+    number: yup
+      .string()
+      .min(3, 'Too short!')
+      .max(50, 'Too long!')
+      .required('Required!'),
+  });
 
   return (
-    <Formik initialValues={{ name: '', number: '' }} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={{ name: '', number: '' }}
+      onSubmit={handleSubmit}
+      validationSchema={validateSchema}
+    >
       <Form>
         <label htmlFor={nameFieldId}>Name</label>
         <Field type="text" name="name" id={nameFieldId} />
+        <ErrorMessage name="name" component="span" style={{ color: 'red' }} />
         <label htmlFor={numberFieldId}>Number</label>
         <Field type="text" name="number" id={numberFieldId} />
+        <ErrorMessage name="name" component="div" style={{ color: 'red' }} />
         <button type="submit">Add contact</button>
       </Form>
     </Formik>
