22fa63e5b8b8a1b18a59b950d8e783818f0365fb
yurii macbook pro <yurikorock777@gmail.com>
2025-08-08 08:00:29 +0300

    add task addcontact

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,17 +10,22 @@ function App() {
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    { id: 'id-5', name: 'Suizie Lander', number: '227-25-80' },
+    { id: 'id-6', name: 'Alex Lanier', number: '898-43-15' },
   ]);
   const [search, setSearch] = useState('');
 
-  const filterContacts = contacts.filter((contact) => 
-    contact.name.toLowerCase().includes(search.toLowerCase())
+  const filterContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(search.toLowerCase()),
   );
+  const addContact = (newContact) => {
+    setContacts((prewContacts) => [...prewContacts, newContact]);
+  };
 
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactForm />
+      <ContactForm onAdd={addContact}/>
       <SearchBox value={search} onSearch={setSearch} />
       <ContactList contacts={filterContacts} />
     </>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,25 @@
 import { Field, Form, Formik } from 'formik';
 import { useId } from 'react';
+import { nanoid } from 'nanoid'
 
-export default function ContactForm() {
-
+export default function ContactForm({ onAdd }) {
   const nameFieldId = useId();
   const numberFieldId = useId();
-  
+
+  const handleSubmit = (values, actions) => {
+    onAdd({
+      id: nanoid(),
+      name: values.name,
+      number: values.number
+    })
+    actions.resetForm();
+  };
+
   return (
-    <Formik>
+    <Formik initialValues={{ name: '', number: '' }} onSubmit={handleSubmit}>
       <Form>
         <label htmlFor={nameFieldId}>Name</label>
-        <Field type="text" name="username" id={nameFieldId} />
+        <Field type="text" name="name" id={nameFieldId} />
         <label htmlFor={numberFieldId}>Number</label>
         <Field type="text" name="number" id={numberFieldId} />
         <button type="submit">Add contact</button>
