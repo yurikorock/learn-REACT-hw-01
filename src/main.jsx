be0b390f493a10423a8de58f4304c8312f5a36bf
yurii macbook pro <yurikorock777@gmail.com>
2025-09-01 20:42:10 +0300

    end persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'modern-normalize';
 import './index.css';
 import App from './components/App/App.jsx';
-import {store} from "./redux/store.js"
+import { store, persistor } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
+
+// як працює PersistGate > read LocaleStorage > set redux state > App
