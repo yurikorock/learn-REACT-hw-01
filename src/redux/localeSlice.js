ab6febf0c668ae2836ea1bd4b5d41219466c3c17
yurii macbook pro <yurikorock777@gmail.com>
2025-09-01 20:03:58 +0300

    end createSlice

--- src/redux/localeSlice.js
+++ src/redux/localeSlice.js
@@ -1,22 +1,38 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
+const slice = createSlice({
+  name: 'locale',
+  initialState: {
+    lang: 'uk',
+  },
+  reducers: {
+    changeLang: (state, action) => {
+      state.lang = action.payload;
+    },
+  },
+});
 
+// console.log(slice);
 
-export const changeLang = createAction('locale/changeLang');
+export const {changeLang} = slice.actions;
 
-const initialState = {
-  lang: "uk",
-};
+export default slice.reducer;
 
-export default function localeReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'locale/changeLang':
-      return {
-        ...state,
-        lang: action.payload,
-      };
-      
-    default:
-      return state;
-  }
-}
+// export const changeLang = createAction('locale/changeLang');
+
+// const initialState = {
+//   lang: "uk",
+// };
+
+// export default function localeReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case 'locale/changeLang':
+//       return {
+//         ...state,
+//         lang: action.payload,
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,46 +3,6 @@ import balanceReducer from './balanceSlice.js';
 import localeReducer from './localeSlice.js';
 
 
-
-// const action = deposit(5);
-
-// const initialState = {
-//   balance: {
-//     value: 100,
-//   },
-//   locale: {
-//     lang: 'uk',
-//   },
-// };
-//функція зміни стану
-// function rootReducer(state = initialState, action) {
-//   switch (action.type) {
-//     case 'balance/deposit':
-//       return {
-//         ...state, //робимо завжди копію існуючого стану
-//         balance: {
-//           value: state.balance.value + action.payload,
-//         },
-//       };
-//     case 'balance/withdraw':
-//       return {
-//         ...state,
-//         balance: {
-//           value: state.balance.value - action.payload,
-//         },
-//       };
-//     case 'locale/changeLang':
-//       return {
-//         ...state,
-//         locale: {
-//           lang: action.payload,
-//         },
-//       };
-//     default:
-//       return state;
-//   }
-// }
-
 export const store = configureStore({
   reducer: {
     balance: balanceReducer,//відповідає за властивість стану balance
